450c37b3a6601e0dc4eafc33d9e22719068d4b8f
fraqia <156285153+fraqia@users.noreply.github.com>
2024-04-19 15:57:47 +0300

    Removed query.json files and changed from post method to get method.

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,29 @@
 import requests
 import json
+import sys
 
-api_url = "https://pxdata.stat.fi/PXWeb/api/v1/en/StatFin/icte/statfin_icte_pxt_13vg.px"
+# api_url = "https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/icte/statfin_icte_pxt_13vg.px"
 
-with open('query.json', 'r') as file:
-    json_query = json.load(file)
+# with open('query2.json', 'r') as file:
+#     json_query = json.load(file)
+
+# response = requests.get(api_url, json=json_query)
+
+# if response.status_code == 200:
+#     data = response.json()
+
+#     print(json.dumps(data, indent=4))
+# else:
+#     print("Failed to fetch data from API:", response.status_code)
+#     print("Response content:", response.content)
+
+api_url = "https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/icte/statfin_icte_pxt_13vg.px"
+
+response = requests.get(api_url)
+
+if not response.ok:
+    print(f"Error: Can't download list from Tilastokeskus: {response.reason}")
+    sys.exit(1)
+
+data = response.json()
+print(data)
