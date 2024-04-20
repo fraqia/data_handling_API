9ab6e4703bd8c128d076d0b79623589f15e1fa13
fraqia <156285153+fraqia@users.noreply.github.com>
2024-04-20 15:21:20 +0300

    into Python dict, added pprint

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from pprint import pprint
 
 api_url = "https://api.tvmaze.com/singlesearch/shows"
 params = {"q":"Girls"}
@@ -7,6 +8,8 @@ params = {"q":"Girls"}
 response = requests.get(api_url, params)
 
 if response.status_code == 200:
-    print(response.text)
+    data = json.loads(response.text)
+    pprint(data)
+
 else:
     print(f'Error: {response.status_code}')
