0283ba40ef8422af4c131c6963d0e9ecb6c37d87
fraqia <156285153+fraqia@users.noreply.github.com>
2024-04-20 15:25:37 +0300

    print out name, premiered, summary

--- data_handler.py
+++ data_handler.py
@@ -3,13 +3,18 @@ import json
 from pprint import pprint
 
 api_url = "https://api.tvmaze.com/singlesearch/shows"
-params = {"q":"Girls"}
+params = {"q":"Star Wars Clone Wars"}
 
 response = requests.get(api_url, params)
 
 if response.status_code == 200:
     data = json.loads(response.text)
-    pprint(data)
+    # pprint(data)
+    name = data['name']
+    premiered = data['premiered']
+    summary = data['summary']
+    print(f"{name} premiered on {premiered}")
+    print(summary)
 
 else:
     print(f'Error: {response.status_code}')
