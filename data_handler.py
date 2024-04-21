9357cde1eebcbab70715e5fac39d4b7294fbf694
fraqia <156285153+fraqia@users.noreply.github.com>
2024-04-21 16:32:05 +0300

    Added encoding

--- data_handler.py
+++ data_handler.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 api_url = "https://pxdata.stat.fi:443/PXWeb/api/v1/en/StatFin/icte/statfin_icte_pxt_13vg.px"
 
-with open('query.json', 'r') as file:
+with open('query.json', 'r', encoding='utf-8') as file:
     json_query = json.load(file)
 
 response = requests.post(api_url, json=json_query)
